f9d71154675974882bbf9b5a3198175baa326e87
Gökalp Avcu <avcu219@gmail.com>
2021-03-21 18:17:56 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //IEntity implement eden class bir veritabanı tablosudur
+    public interface IEntity
+    {
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity
+    {
+        public int ProductID { get; set; }
+        public object ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+
+        public decimal UnitPrice { get; set; }
+    }
+
+}
